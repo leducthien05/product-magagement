e5a37e5af7a55c466e44818eb652623c1f40176e
le Duc Thien <ducthien09092005@gmail.com>
2025-05-21 15:05:55 +0700

    bổ sung phần sắp xếp sản phẩm theo tiêu chí

--- public/admin/JS/product-category.js
+++ public/admin/JS/product-category.js
@@ -1,6 +1,5 @@
 //Trạng thái sản phẩm
 const buttonStatus = document.querySelectorAll("[button-status]");
-console.log(buttonStatus);
 if(buttonStatus){
     buttonStatus.forEach(item =>{
         let url = new URL(window.location.href);
@@ -50,4 +49,32 @@ if(pagination){
     });
 }
 
-//Xóa sản phẩm
+//Sắp xếp sản phẩm theo tiêu chí
+const divsort = document.querySelector("[sortCategory]");
+if(divsort){
+    let url = new URL(window.location.href);
+    const selected = document.querySelector("[sort-select-category]");
+    const sortClear = document.querySelector("[sort-clear-category]");
+    selected.addEventListener("change", (e)=>{
+        const value = e.target.value;
+        let [sortKey, valueKey] = value.split("-");
+        url.searchParams.set("sortKey", sortKey);
+        url.searchParams.set("valueKey", valueKey);
+        window.location.href = url.href;
+    });
+    sortClear.addEventListener("click",()=> {
+        url.searchParams.delete("sortKey");
+        url.searchParams.delete("valueKey");
+        window.location.href = url.href;
+    });
+
+    const value1 = url.searchParams.get("sortKey");
+    const value2 = url.searchParams.get("valueKey");
+    const sortString = `${value1}-${value2}`;
+    if(sortString){
+        const option = selected.querySelector(`option[value='${sortString}']`);
+        if(option){
+            option.selected = true;
+        }
+    }
+}
