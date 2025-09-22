9e123552767cb89a6ad2423b5db327338c9e0b4f
le Duc Thien <ducthien09092005@gmail.com>
2025-09-22 15:50:31 +0700

    Tạo conllection và model trang thanh toán

---
+++ model/oder.model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+    //ID_User: String,
+    ID_Cart: String,
+    userInfo: {
+        fullName: String,
+        phone: String,
+        address: String
+    },
+    products:[
+        {
+            product_ID: String,
+            price: Number,
+            discountPercentage: Number,
+            quantity: Number
+        }
+    ],
+    deleted: {
+        type: Boolean,
+        default: false
+    },
+    deletedAt: Date
+},
+{
+    timestamps: true
+});
+   
+
+const Order = mongoose.model('Order', orderSchema, "order");
+//kết nối ra bên ngoài
+module.exports = Order;
